6c8d92af58680b6dae59273dcda9d00ce119a92f
Federico <caceresfederico@hotmail.com>
2023-02-15 17:30:40 -0300

    comments and titles added to level 1 & level 2 exercises

--- Day05_Arrays/level1.js
+++ Day05_Arrays/level1.js
@@ -1,15 +1,19 @@
-// ** LEVEL 1 CHALLENGES **
+//                                                                        ** LEVEL 1 CHALLENGES **
 // 1
 const myArray = [];
 const myArrayConstructor = Array();
+
 // 2
 const friends = ["Lucas", "Dennis", "Javier", "Pablo", "Facundo"];
+
 // 3
 const friendsLen = friends.length; // 4
+
 // 4
 const firstFriend = friends[0]; // Lucas
 const middleFriend = friends[parseInt(friends.length / 2)]; //Javier
 const lastFriend = friends[friends.length - 1]; // Facundo
+
 // 5
 const mixedDataTypes = [
   "Logitech",
@@ -20,6 +24,7 @@ const mixedDataTypes = [
   false,
 ];
 const mixedDataTypesLength = mixedDataTypes.length; // 6
+
 // 6
 const itCompanies = [
   "Facebook",
@@ -30,60 +35,78 @@ const itCompanies = [
   "Oracle",
   "Amazon",
 ];
+
 // 7
 console.log(itCompanies); // ['Facebook','Google','Microsoft', 'Apple', 'IBM', 'Oracle','Amazon']
+
 // 8
 console.log(itCompanies.length); //7
+
 // 9
 const firstCompany = itCompanies[0]; // Facebook
 const middleCompany = itCompanies[parseInt(itCompanies.length / 2)]; //4
 const lastCompany = itCompanies[itCompanies.length - 1]; // Amazon
+
 // 10
 const printCompany = itCompanies.map((company) => console.log(company));
 // ['Facebook','Google','Microsoft', 'Apple', 'IBM', 'Oracle','Amazon']
+
 // 11
 const itCompaniesToUpperCase = itCompanies.map((company) =>
   console.log(company.toLocaleUpperCase())
-);
-// ['FACEBOOK', 'GOOGLE', 'MICROSOFT', 'APPLE', 'IBM', 'ORACLE', 'AMAZON']
+); // ['FACEBOOK', 'GOOGLE', 'MICROSOFT', 'APPLE', 'IBM', 'ORACLE', 'AMAZON']
 
-// 12 
-const sentence = [...itCompanies]
-sentence.pop()
-sentence.push('and Amazon are Big It Companies')
-console.log(sentence.join(' '))
+// 12
+const sentence = [...itCompanies];
+sentence.pop();
+sentence.push("and Amazon are Big It Companies");
+console.log(sentence.join(" "));
 // "Facebook,Google,Microsoft,Apple,IBM,Oracle,and Amazon are Big It Companies"
+
 // 13
 const companyToSearch = "Amazon";
 const searchCompany = itCompanies.includes(companyToSearch); // true
+
 // 14
 const result = [];
 const searchCompanyMap = itCompanies.map((company) => {
   if (company.includes("oo")) {
     result.push(company);
   }
-});
-// [ 'Facebook', 'Google' ]
+});// [ 'Facebook', 'Google' ]
+
 // 15
-const sortItCompanies = itCompanies.sort()
+const sortItCompanies = itCompanies.sort();
 // ["Amazon", "Apple", "Facebook", "Google", "IBM", "Microsoft", "Oracle"]
+
 // 16
-const reverseItCompanies = itCompanies.reverse()
+const reverseItCompanies = itCompanies.reverse();
 // ['Oracle','Microsoft','IBM','Google','Facebook','Apple','Amazon']
+
 // 17
 const firstThreeCompanies = itCompanies.slice(0, 3); // [ 'Oracle', 'Microsoft', 'IBM' ]
+
 // 18
-const lastThreeCompanies = itCompanies.slice(itCompanies.length-3,itCompanies.length)
-// ['Facebook', 'Apple', 'Amazon']
+const lastThreeCompanies = itCompanies.slice(
+  itCompanies.length - 3,
+  itCompanies.length
+); // ['Facebook', 'Apple', 'Amazon']
+
 // 19
-const middleCompanySlice = itCompanies.slice(parseInt(itCompanies.length/2),Math.ceil(itCompanies.length/2))
-// ['Google']
+const middleCompanySlice = itCompanies.slice(
+  parseInt(itCompanies.length / 2),
+  Math.ceil(itCompanies.length / 2)
+); // ['Google']
+
 // 20
-const removeFirstCompany = itCompanies.shift()
+const removeFirstCompany = itCompanies.shift();
 // [ 'Microsoft', 'IBM', 'Google', 'Facebook', 'Apple', 'Amazon' ] ORACLE REMOVED
+
 // 21
-const removeMiddleCompanies = itCompanies.slice(2,4)
+const removeMiddleCompanies = itCompanies.slice(2, 4);
+
 // 22
-const removeLastCompany = itCompanies.pop()
+const removeLastCompany = itCompanies.pop();
+
 // 23
-const removeAllCompanies = itCompanies.splice()
+const removeAllCompanies = itCompanies.splice();
--- Day05_Arrays/level2/main.js
+++ Day05_Arrays/level2/main.js
@@ -1,49 +1,65 @@
-// 1
+//                                                                        ** LEVEL 2 CHALLENGES **
+/* 1-  Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. 
+Access both file in main.js file   */
 import { countries } from "./countries.js";
 import { webtechs } from "./webtechs.js";
 
-// 2
+
+// 2- First remove all the punctuations and change the string to array and count the number of words in the array
 const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
 let text =
   "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
 let cleanText = text.replace(regex, ""); // With regex, you can replace all matches with an empty string:
 let textArray = cleanText.split(" ");
-// console.log(textArray);
+console.log(textArray);
 // ["I","love","teaching","and","empowering","people","I","teach","HTML","CSS","JS","React","Python"]
 // console.log(textArray.length); // 13
 
 
-// 3
+// 3- In the following shopping cart add, remove, edit items
+// 3.1 - Add 'Meat' in the beginning of your shopping cart if it has not been already added
 const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
 shoppingCart.includes("Meat")
   ? console.log("Meat was already added")
   : shoppingCart.unshift("Meat");
 // [ 'Meat', 'Milk', 'Coffee', 'Tea' ]
 
+// 3.2 - Add Sugar at the end of you shopping cart if it has not been already added
 shoppingCart.includes("Sugar")
   ? console.log("Sugas was already added")
   : shoppingCart.push("Sugar");
-// [ 'Meat', 'Milk', 'Coffee', 'Tea', 'Honey', 'Sugar' ]°
+// [ 'Meat', 'Milk', 'Coffee', 'Tea', 'Honey', 'Sugar' ]
 
+// 3.3 - Remove 'Honey' if you are allergic to honey
 const alergicToHoney = true;
 alergicToHoney
   ? shoppingCart.splice(shoppingCart.indexOf("Honey"), 1)
   : console.log("Not alergic to honey");
 // [ 'Meat', 'Milk', 'Coffee', 'Tea', 'Sugar' ]
 
-shoppingCart[shoppingCart.indexOf('Tea')] = 'Green Tea'
+// 3.4 - Modify Tea to 'Green Tea'
+shoppingCart[shoppingCart.indexOf("Tea")] = "Green Tea";
 // [ 'Meat', 'Milk', 'Coffee', 'Green Tea', 'Sugar' ]
 
 
-// 4
-const countryToFound = 'Ethiopia'
+// 4 In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+const countryToFound = "Ethiopia";
 countries.includes(`${countryToFound}`)
   ? console.log(`${countryToFound.toUpperCase()}`)
-  : countries.push(`${countryToFound}`) 
+  : countries.push(`${countryToFound}`);
 // Ethiopia alredy exist
 
-const webTechToFound = 'Sass'
+
+// 5- In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+const webTechToFound = "Sass";
 webtechs.includes(`${webTechToFound}`)
   ? console.log(`${webTechToFound} is a CSS preprocess`)
-  : webtechs.push(webTechToFound)
+  : webtechs.push(webTechToFound);
 // SASS doesn't exist
+
+// 6- Concatenate the following two variables and store it in a fullStack variable.
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = frontEnd.concat(backEnd)
+console.log(fullStack) 
+// ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
